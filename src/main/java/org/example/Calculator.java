d03389b1ce0e59d2b3e9195775e6ce000371aed9
DmitryKravchenko1 <st4028025@stud.duikt.edu.ua>
2025-11-18 12:44:34 +0200

    some changes

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -17,14 +17,14 @@ public class Calculator {
 
     public static double divide(double a, double b) throws ArithmeticException, InvalidInputException {
         if (b == 0.0) {
-            throw new ArithmeticException("Не можна ділити на нуль!");
+            throw new ArithmeticException("Ділити на нуль не можна.");
         }
         return a / b;
     }
 
     public static double sqrt(double a) throws InvalidInputException {
         if (a < 0) {
-            throw new InvalidInputException("Не можна обчислити корінь квадратний з від’ємного числа!");
+            throw new InvalidInputException("Обчислювати корінь квадратний з від’ємного числа не можна.");
         }
         return Math.sqrt(a);
     }
